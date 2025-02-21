436218e8c91e85653e09442a50fb336fd1752f5f
jasvix12 <jasvialvarez@gmail.com>
2025-02-21 09:02:56 -0500

    icon_google

--- lib/notification_page.dart
+++ lib/notification_page.dart
@@ -8,6 +8,7 @@ class NotificationPage extends StatefulWidget {
 
 class _NotificationPageState extends State<NotificationPage> {
   final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
+  String? _token;
 
   @override
   void initState() {
@@ -18,6 +19,9 @@ class _NotificationPageState extends State<NotificationPage> {
 
     // Obtener el token de FCM (para pruebas)
     _firebaseMessaging.getToken().then((token) {
+      setState(() {
+        _token = token;
+      });
       print("Token de FCM: $token");
     });
 
@@ -31,12 +35,43 @@ class _NotificationPageState extends State<NotificationPage> {
     });
   }
 
+  Future<void> _requestPermission() async {
+    NotificationSettings settings = await _firebaseMessaging.requestPermission(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
+
+    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Permisos concedidos")),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Permisos denegados")),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Notificaciones")),
-      body: Center(child: Text("Esperando notificaciones...")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Esperando notificaciones..."),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _requestPermission,
+              child: Text("Solicitar Permisos"),
+            ),
+            SizedBox(height: 20),
+            if (_token != null) Text("Token de FCM: $_token"),
+          ],
+        ),
+      ),
     );
   }
 }
-
