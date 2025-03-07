86e1f49d6d2f6ffb4f065b8da2333f294a340833
jasvix12 <jasvialvarez@gmail.com>
2025-03-07 11:50:46 -0500

    07/03/2025

--- lib/send_notification_page.dart
+++ lib/send_notification_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class SendNotificationPage extends StatefulWidget {
   @override
@@ -11,8 +12,22 @@ class _SendNotificationPageState extends State<SendNotificationPage> {
 
   Future<void> _sendNotification() async {
     String message = _messageController.text.trim();
-    if (message.isEmpty) return;
+    if (message.isEmpty){
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("El mensaje no puede estar vacio")),
+      );
+      return;
+    }
 
+    final user = FirebaseAuth.instance.currentUser;
+      if (user == null) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text("Debes iniciar sesion para enviar notificaciones")),
+        );
+        return;
+      }
+    
+    try{
     await FirebaseFirestore.instance.collection('notifications').add({
       'message': message,
       'timestamp': FieldValue.serverTimestamp(),
@@ -24,7 +39,13 @@ class _SendNotificationPageState extends State<SendNotificationPage> {
     );
 
     _messageController.clear();
+  } catch (e) {
+    print("Error al enviar notificacion: $e");
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar (content: Text("Error al enviar notificacion")),
+    );
   }
+}
 
   @override
   Widget build(BuildContext context) {
